1b1459ea277391b7f8d010734c2978ec0f6a68ff
aanandunnis <66952088+aanandunnis@users.noreply.github.com>
2021-10-03 21:11:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+import os
 
-app = Flask(__name__)
+TEMPLATE_DIR = os.path.abspath('./templates')
+STATIC_DIR = os.path.abspath('./static')
+
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 
 @app.route('/')
 
