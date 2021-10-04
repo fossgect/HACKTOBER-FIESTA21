133387f284f245a55a04a0097fca5e2826d0fc17
ben-jnr <bichubenkuruvilla@gmail.com>
2021-10-04 21:49:28 +0530

    home page design modified

--- static/main/script.js
+++ static/main/script.js
@@ -1,16 +1,17 @@
-let list = $("#list");
+let list = $(".list");
 
 function anim() {
     let st = list.scrollTop()+list.height();
-    let sb = list.prop("scrollHeight")
+    let sb = list.prop("scrollHeight");
     let diff = sb - st;
     if(diff <= 1 & diff >= -1) {
-        list.animate({scrollTop: 0}, 0, anim);
+        list.animate({scrollTop: 0}, 0, "linear", anim);
     }
     else {
-        list.animate({scrollTop: sb}, (sb-st)*10, anim);
+        list.animate({scrollTop: sb}, (sb-st)*15, "linear", anim);
     }
 }
+
 anim();
 
 list.mouseenter(
