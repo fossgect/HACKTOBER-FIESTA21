c1e82eda22ed80591e420b10e3280e3e0311aefe
ben-jnr <bichubenkuruvilla@gmail.com>
2021-10-06 13:26:14 +0530

    removed unnecessary paths for templates

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, url_for
 import os
 
-TEMPLATE_DIR = os.path.abspath('./templates')
-STATIC_DIR = os.path.abspath('./static')
-app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
